93f779855b932dceccc7b9c74192cb00266e4e1d
m-a-r-o-u <58525783+m-a-r-o-u@users.noreply.github.com>
2025-11-13 21:22:50 +0100

    Add sim-app command to collect AI system user emails

--- src/sim_app/__init__.py
+++ src/sim_app/__init__.py
@@ -1,8 +1,16 @@
 """High-level SIM app utilities built on top of the SIM API wrapper."""
 
+from .ai_systems import (
+    AiSystemsCollectionError,
+    AiSystemsEmailCollectionResult,
+    collect_ai_system_user_emails,
+)
 from .mcml import McmlEmailCollectionResult, collect_mcml_master_user_emails
 
 __all__ = [
+    "AiSystemsCollectionError",
+    "AiSystemsEmailCollectionResult",
+    "collect_ai_system_user_emails",
     "McmlEmailCollectionResult",
     "collect_mcml_master_user_emails",
 ]
